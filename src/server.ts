f534d7030ef96f2180eb57896bf5d59878fd884d
Élise <elise@eiss.fr>
2026-02-20 10:47:29 +0100

    fixed introduced typo

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = buildApp();
 
 app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
   if (err) {
-    console.erro(err);
+    console.error(err);
     process.exit(1);
   }
   console.log(`Serveur démarré sur ${address}`);
